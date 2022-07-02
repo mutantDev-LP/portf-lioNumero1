08cfd87de98cba6f3e39ba0b8c72a5b160884cf2
Paulo Lucio <71181388+mutantDev-LP@users.noreply.github.com>
2022-07-02 19:12:15 -0300

    Add files via upload

---
+++ src/register/partials/script.js
@@ -0,0 +1,8 @@
+export let nome = document.getElementById("nome");
+export let email = document.getElementById("email");
+
+import { createUser } from "../../../modules/express";
+
+function registerUser() {
+  createUser;
+}
